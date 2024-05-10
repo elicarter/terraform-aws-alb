305db6598cbcb44d6faa168b1de11ca620fd0164
elicarter <89507487+elicarter@users.noreply.github.com>
2024-05-10 08:53:41 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,26 @@
-resource "aws_lb" "my_lb" {
-  name               = "my-load-balancer"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.elb_sg.id]
- subnets = [for subnet in aws_subnet.my_subnet : subnet.id]
+resource "aws_lb_target_group" "my_target_group" {
+  name     = "my-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.existing_vpc_id
 
-tags = {
-    Name = "my-load-balancer"
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "my_listener" {
+  load_balancer_arn = aws_lb.my_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.my_target_group.arn
   }
 }
