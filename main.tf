3a4267e8eecebc138ac2a4b5fef1914588117d96
elicarter <89507487+elicarter@users.noreply.github.com>
2024-05-10 08:52:08 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
+resource "aws_lb" "my_lb" {
+  name               = "my-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.elb_sg.id]
+ subnets = [for subnet in aws_subnet.my_subnet : subnet.id]
 
+tags = {
+    Name = "my-load-balancer"
+  }
+}
