49b8fe7d83a5cc9a1a5cfe90e90e2b1a640d1c6f
elicarter <89507487+elicarter@users.noreply.github.com>
2024-05-10 09:01:19 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+resource "aws_lb" "my_lb" {
+  name               = "my-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.elb_sg.id]
+ subnets = [for subnet in aws_subnet.my_subnet : subnet.id]
+
+tags = {
+    Name = "my-load-balancer"
+  }
+}
+
+
 resource "aws_lb_target_group" "my_target_group" {
   name     = "my-target-group"
   port     = 80
